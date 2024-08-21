6599b94abeb023d657922e8b9c24d2475d2c1bfa
kay-GitHubMember <162615933+kay-GitHubMember@users.noreply.github.com>
2024-08-21 18:16:55 +0300

    Update LevelUp.jsx

--- components/Main/LevelUp/LevelUp.jsx
+++ components/Main/LevelUp/LevelUp.jsx
@@ -1,18 +1,21 @@
 import styles from '../LevelUp/LevelUp.module.scss'
 import upgrade_image from '../../../src/assets/upgrade-image.png'
 import { useAuth } from '../../../hooks/useAuth'
+import { useState } from 'react'
 
 export function LevelUp() {
     const { numberOfCoinsIncreased, setNumberOfCoinsIncreased } = useAuth()
     const { coins, setCoins } = useAuth()
+    const [priceOfUpgrade, setPriceOfUpgrade] = useState(2500)
 
     const handleButtonClick = () => {
-        if (coins < 2500) {
+        if (coins < priceOfUpgrade) {
             setCoins((prevCoins) => prevCoins + 0)
         }
         else {
             setNumberOfCoinsIncreased((prevCoins) => prevCoins + 3)
-            setCoins((prevCoins) => prevCoins - 2500)
+            setPriceOfUpgrade((prevValue) => prevValue * 2)
+            setCoins((prevCoins) => prevCoins - priceOfUpgrade)
         }
     }
 
@@ -25,7 +28,7 @@ export function LevelUp() {
                     <p className={styles.upgrade_text}>Увеличить количество монет на 3</p>
 
                     <button className={styles.upgrade_button} onClick={handleButtonClick}>
-                        2500
+                        {priceOfUpgrade}
                     </button>
                 </li>
             </ul>
