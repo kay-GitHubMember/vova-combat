2a1f59907ff159c87296435c1b919c673de0c994
kay-GitHubMember <162615933+kay-GitHubMember@users.noreply.github.com>
2024-08-21 18:16:34 +0300

    Update Main.jsx

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -4,10 +4,18 @@ import lesha_coin from '../../src/assets/lesha-coin.png'
 import { useEffect, useState } from 'react'
 import { LevelUp } from './LevelUp/LevelUp'
 import { useAuth } from '../../hooks/useAuth'
+import { useRef } from 'react'
 
 export function Main() {
     const { coins, setCoins } = useAuth()
     const { numberOfCoinsIncreased, setnumberOfCoinsIncreased } = useAuth()
+    const numberOfCoinsRef = useRef(null)
+
+    useEffect(() => {
+        if (coins > 10000000000) {
+            numberOfCoinsRef.current.style.fontSize = "45px"
+        }
+    })
 
     const handleButtonClick = () => {
         setCoins((prevCoins) => prevCoins + numberOfCoinsIncreased)
@@ -20,7 +28,7 @@ export function Main() {
                     <img src={lesha_coin} alt="" className={styles.lesha_coin} />
                 </div>
                 <input type="text" className={styles.number_of_coins} 
-                value={coins}/>
+                value={coins} ref={numberOfCoinsRef} />
             </div>
             <button className={styles.vova_place} onClick={handleButtonClick}>
                 <img src={vova_image} alt="" className={styles.vova_image} />
